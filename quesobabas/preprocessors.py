9907922998c1fb513e25a114f6105cf7b3c4dad5
Luis Alfredo Lorenzo <babasbot@gmail.com>
2018-10-26 14:38:16 -0500

    Removes the bot's telegram username from text

--- quesobabas/preprocessors.py
+++ quesobabas/preprocessors.py
@@ -4,5 +4,6 @@ logging.basicConfig(level=logging.INFO)
 
 def clean_screen_name(chatbot, statement):
     statement.text = statement.text.replace("@ilovequesobabas", "")
+    statement.text = statement.text.replace("@QuesoBabasBot ", "")
 
     return statement
